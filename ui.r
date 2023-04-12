859c06ac2c77e1bc1dca2f5c101c34ef455c2647
Ricardo Bizerra <ricardo.bizerra19@gmail.com>
2023-04-12 18:33:52 -0300

    feature(main): ui improvements

--- ui.r
+++ ui.r
@@ -1,6 +1,10 @@
 # Define UI
 ui <- dashboardPage(
-  dashboardHeader(title = "Análise de Séries Temporais"),
+  skin = "green",
+  dashboardHeader(
+    title = "Análise da ação da Starbucks",
+    titleWidth = 300
+  ),
   dashboardSidebar(
     sidebarMenu(
       menuItem("Análise de uma classe", tabName = "tab1", icon = icon("line-chart")),
@@ -13,6 +17,23 @@ ui <- dashboardPage(
       # Aba 1 - Análise de uma classe
       tabItem(
         tabName = "tab1",
+        fluidRow(
+          box(
+            status = "primary",
+            background = "green",
+            width = 12,
+            tags$b("Análise de características individuais da ação da Starbucks (SBUX), listada no índice Nasdaq, entre 1992 e 2021"),
+            p("Legenda das séries temporais:"),
+            tags$ul(
+              tags$li("Open: Preço da ação no momento da abertura do pregão"),
+              tags$li("High: Preço mais alto alcançado pela ação no pregão"),
+              tags$li("Low: Preço mais baixo alcançado pela ação no pregão"),
+              tags$li("Close: Preço da ação no momento do fechamento do pregão"),
+              tags$li("Volume: Quantidade de movimentações (compras e vendas de ações)"),
+              tags$li("Dividends: Participação no lucro da empresa (lucro destinado à divisão entre acionistas dividido pelo número de ações)"),
+            )
+          )
+        ),
         fluidRow(
           box(
             title = "Inputs",
@@ -34,12 +55,41 @@ ui <- dashboardPage(
             plotOutput("histograma"),
             plotOutput("boxplot")
           )
+        ),
+        fluidRow(
+          box(
+            status = "primary",
+            width = 12,
+            tags$b("Participantes do grupo:"),
+            tags$ul(
+              tags$li("João Luís Gomes Agra (jlga)"),
+              tags$li("João Victor Ribeiro Costa de Omena (jvrco)"),
+              tags$li("Ricardo Bizerra de Lima Filho (rblf)")
+            )
+          )
         )
       ),
 
       # Aba 2 - Análise de Duas Classes
       tabItem(
         tabName = "tab2",
+        fluidRow(
+          box(
+            status = "primary",
+            background = "green",
+            width = 12,
+            tags$b("Análise de duplas de características da ação da Starbucks (SBUX), listada no índice Nasdaq, entre 1992 e 2021"),
+            p("Legenda das séries temporais:"),
+            tags$ul(
+              tags$li("Open: Preço da ação no momento da abertura do pregão"),
+              tags$li("High: Preço mais alto alcançado pela ação no pregão"),
+              tags$li("Low: Preço mais baixo alcançado pela ação no pregão"),
+              tags$li("Close: Preço da ação no momento do fechamento do pregão"),
+              tags$li("Volume: Quantidade de movimentações (compras e vendas de ações)"),
+              tags$li("Dividends: Participação no lucro da empresa (lucro destinado à divisão entre acionistas dividido pelo número de ações)"),
+            )
+          )
+        ),
         fluidRow(
           box(
             title = "Inputs",
@@ -61,6 +111,18 @@ ui <- dashboardPage(
             plotOutput("grafico_linha2"),
             plotOutput("grafico_barra"),
             plotOutput("scatterplot")
+          )
+        ),
+        fluidRow(
+          box(
+            status = "primary",
+            width = 12,
+            tags$b("Participantes do grupo:"),
+            tags$ul(
+              tags$li("João Luís Gomes Agra (jlga)"),
+              tags$li("João Victor Ribeiro Costa de Omena (jvrco)"),
+              tags$li("Ricardo Bizerra de Lima Filho (rblf)")
+            )
           )
         )
       )
